61d64c11f59cea6d6ca9661d3e7df7412c11a11b
accLukePex <luca.pessione@accenture.com>
2024-06-27 15:59:10 +0200

    added: Footer Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/TopBar/Navbar';
 import Jumbotron from './components/Jumbotron/Jumbotron';
 import MainComponent from './components/Main/MainComponent';
+import Bottombar from './components/BottomBar/Bottombar';
 
 function App() {
     return (
@@ -14,7 +15,7 @@ function App() {
                 <MainComponent />
             </main>
             <footer>
-                
+                <Bottombar />
             </footer>
         </div>
     );

a8680a101a33c2634a6f8432c7015800c7453a57
accLukePex <luca.pessione@accenture.com>
2024-07-02 10:59:39 +0200

    added: initial Loader to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Loader from "./components/Main/Loader/Loader";
 import Navbar from "./components/TopBar/Navbar";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
 import MainComponent from "./components/Main/MainComponent";
 import Bottombar from "./components/BottomBar/Bottombar";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    // Simula il caricamento iniziale con un ritardo
+    setTimeout(() => {
+      loadCards();
+    }, 2000); // 2 secondi di ritardo
+  }, []);
+
+  const loadCards = () => {
+    const url = "https://api.thecatapi.com/v1/images/search?limit=10";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCats(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching the cat images:", error);
+        setLoading(false);
+      });
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className="app">
       <header>
@@ -12,7 +42,7 @@ function App() {
         <Jumbotron />
       </header>
       <main>
-        <MainComponent />
+        <MainComponent cats={cats} />
       </main>
       <footer>
         <Bottombar />
--- src/components/Main/MainComponent.jsx
+++ src/components/Main/MainComponent.jsx
@@ -1,28 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./MainComponent.css";
+import Loader from "./Loader/Loader";
 import "./Button.css";
 import Container from "../Container";
 import Cardlist from "./Cardlist/Cardlist";
 
-function MainComponent() {
-  const [cats, setCats] = useState([]);
-  const [loading, setLoading] = useState(true);
+function MainComponent({ cats: initialCats }) {
+  const [cats, setCats] = useState(initialCats);
+  const [loadingMore, setLoadingMore] = useState(false);
 
-  useEffect(() => {
-    loadCards();
-  }, []);
-
-  const loadCards = () => {
+  const loadMoreCards = () => {
+    setLoadingMore(true);
     const url = "https://api.thecatapi.com/v1/images/search?limit=10";
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setCats((prevCats) => [...prevCats, ...data]);
-        setLoading(false);
+        setLoadingMore(false);
       })
       .catch((error) => {
         console.error("Error fetching the cat images:", error);
-        setLoading(false);
+        setLoadingMore(false);
       });
   };
 
@@ -34,26 +32,25 @@ function MainComponent() {
     );
   };
 
-  const loadMoreCards = () => {
-    setLoading(true);
-    loadCards();
-  };
-
   return (
-    <Container>
-      <div className="box-title">
-        <h2>Gatti in vendita a Milano</h2>
-      </div>
-      <div className="box-sub-title">
-        <h3>230 Gatti in vendita a Milano</h3>
-      </div>
+    <div className="main-container">
+      <Container>
+        <div className="content-container">
+          <div className="box-title">
+            <h2>Gatti in vendita a Milano</h2>
+          </div>
+          <div className="box-sub-title">
+            <h3>230 Gatti in vendita a Milano</h3>
+          </div>
+          <Cardlist cats={cats} />
 
-      <Cardlist cats={cats} />
-
-      <div className="load-more-container">
-        {!loading && <LoadMoreButton onClick={loadMoreCards} />}
-      </div>
-    </Container>
+          <div className="load-more-container">
+            {!loadingMore && <LoadMoreButton onClick={loadMoreCards} />}
+            {loadingMore && <Loader />}
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 }
 
